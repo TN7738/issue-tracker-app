6659010a1cd1e1f0fb8987df3742c07bc1ad740d
Tejas Nashikkar <tj.tejas77@gmail.com>
2024-02-13 19:59:22 -0500

    week 6 - part 1

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 const app = express();
 
 connectDB();
+
+app.use(cors());
 
 app.use(
     "/graphql",
